756d5fa91299e091bbf66e2a5c585ed518b03e73
Rafaelbilly <rafaelbilly6@gmail.com>
2023-10-21 19:30:28 +0700

    first commit

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,443 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Semesta Group</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <!-- Bootstrap 5 -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
+
+    <style>
+        html {
+            scroll-behavior: smooth;
+        }
+
+        body {
+            font-family: "Inter", sans-serif;
+            font-weight: normal;
+            font-style: normal;
+            overflow-x: hidden;
+            font-size: 15px;
+        }
+
+        p {
+            margin: 0;
+            padding: 0;
+            font-size: 15px;
+            line-height: 24px;
+        }
+
+        .navbar {
+            background-color: transparent;
+            /* Set the background color of the navbar to transparent */
+            transition: background-color 0.5s ease-in-out;
+            /* Add a transition effect to smoothly change the background color */
+            position: fixed;
+            /* Set the navbar to fixed position so that it stays at the top */
+            width: 100%;
+            /* Set the width of the navbar to 100% */
+            z-index: 1000;
+            /* Set a high z-index to ensure the navbar is displayed on top of other elements */
+        }
+
+        .navbar.scroll {
+            background-color: rgba(0, 0, 0, 0.5);
+            /* Set the background color of the navbar to a semi-transparent color when scrolling */
+        }
+
+        .hero-area {
+            position: relative;
+            padding: 180px 0 120px 0;
+            background: #303030;
+        }
+
+        .hero-area .hero-image img {
+            width: 100%;
+        }
+
+        .hero-area .hero-content {
+            border-radius: 0;
+            position: relative;
+            z-index: 1;
+            text-align: left;
+        }
+
+        .hero-area .hero-content h1 {
+            font-size: 45px;
+            font-weight: 800;
+            line-height: 50px;
+            color: #fff;
+            text-shadow: 0px 3px 8px #00000017;
+            text-transform: capitalize;
+        }
+
+        .hero-area .hero-content h1 span {
+            display: block;
+        }
+
+        .hero-area .hero-content p {
+            margin-top: 30px;
+            font-size: 20px;
+            color: #fff;
+        }
+
+        .about {
+            padding: 70px 0;
+        }
+
+        .about img {
+            width: 100%;
+            height: auto;
+        }
+
+        .about h2 {
+            font-size: 3rem;
+            margin-bottom: 20px;
+        }
+
+        .about p {
+            font-size: 1.5em;
+            margin-bottom: 20px;
+        }
+
+        .what-we-do {
+            padding: 50px 0;
+        }
+
+        .what-we-do-item {
+            text-align: center;
+            padding: 20px;
+        }
+
+        .what-we-do-item i {
+            font-size: 3em;
+            color: #000;
+        }
+
+        .what-we-do-item h3 {
+            font-size: 2em;
+            margin-top: 0;
+        }
+
+        .what-we-do-item p {
+            font-size: 1.5em;
+        }
+
+        .team {
+            height: 500px;
+        }
+
+        .card-group {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 20px;
+        }
+
+        .card {
+            background-color: #E2E6ED;
+        }
+
+        .card:hover {
+            transform: scale(1.1);
+            transition: transform 0.5s;
+        }
+
+        footer.footer-no-gap {
+            margin-top: 0px;
+        }
+
+        footer {
+            margin-top: 50px;
+            background-color: #303030;
+            min-height: 100px;
+        }
+
+        footer p {
+            text-align: left;
+            line-height: 100px;
+            color: #fff;
+            font-size: 16px;
+            font-weight: 400;
+        }
+
+        footer p a {
+            color: #fff;
+            transition: all .3s;
+            position: relative;
+            z-index: 3;
+        }
+
+        footer p a:hover {
+            opacity: 0.75;
+        }
+
+        .social-media {
+            margin-top: 20px;
+            text-align: center;
+        }
+
+        .social-media a {
+            color: #000;
+            font-size: 20px;
+            margin: 0 10px;
+        }
+
+        .social-media a:hover {
+            color: #333;
+        }
+
+        .partnership {
+            height: 300px;
+            padding: 12px 0;
+            text-align: center;
+        }
+
+        .partnership img {
+            max-width: 50%;
+            transition: all 0.4s ease-in-out;
+            display: inline-block;
+            padding: 15px 0;
+            filter: grayscale(100);
+        }
+
+        .partnership img:hover {
+            filter: none;
+            transform: scale(1.1);
+        }
+
+        .clients {
+            margin-top: 200px;
+            height: 400px;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+        <div class="container">
+            <a class="navbar-brand" href="#"><strong>SemestaGroup</strong></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="mx-auto"></div>
+                <ul class="navbar-nav justify-content-center align-items-center fs-5 flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Booking</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+
+                <div class="btn d-flex justify-content-center align-items-center gap-3">
+                    <a class="btn btn-outline-light" href="{{ url('login') }}" role="button">Login</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Hero -->
+    <section id="home" class="hero-area">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-5 col-md-12 col-12">
+                    <div class="hero-content">
+                        <h1>The Biggest Event Organizer in Indonesia</h1>
+                        <p>We are experts in creating unforgettable moments.</p>
+                    </div>
+                </div>
+                <div class="col-lg-7 col-md-12 col-12">
+                    <div class="hero-image">
+                        <img src="{{ asset('images/carousel-3.jpg') }}" alt="#">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /Hero -->
+
+    <!-- About -->
+    <section class="about">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <img src="{{ asset('images/carousel-1.jpg') }}" alt="about">
+                </div>
+                <div class="col-md-6">
+                    <h1><strong>ABOUT US</strong></h1>
+                    <p>Semesta Group is an event organizer and technical event production. Our services includes event planning and production, design and decoration, lighting and sound system, stage, rigging, entertainment and talent, on-site management, and many more.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /About -->
+
+    <!-- What we do -->
+    <section class="what-we-do">
+        <div class="container">
+            <h1 style="text-align: center;"><strong>WHAT WE DO</strong></h1>
+            <br>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="what-we-do-item">
+                        <i class="fa fa-calendar-alt"></i>
+                        <h3>Event Organizing</h3>
+                        <p>Concept & Creative Development</p>
+                        <p>Production Plan</p>
+                        <p>Event Management</p>
+                    </div>
+                    <div class="what-we-do-item">
+                        <i class="fa fa-microphone"></i>
+                        <h3>Technical Event Production</h3>
+                        <p>We provide all the technical support you need for your event, from lighting and sound to staging and rigging.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="what-we-do-item">
+                        <i class="fa fa-palette"></i>
+                        <h3>Design and Decoration</h3>
+                        <p>We will work with you to create a custom design and décor for your event that will reflect your brand and style.</p>
+                    </div>
+                    <div class="what-we-do-item">
+                        <i class="fa fa-users"></i>
+                        <h3>On-site Management</h3>
+                        <p>We will be on-site to ensure that your event runs smoothly and that your guests have a memorable experience.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Parthership -->
+    <section class="partnership">
+        <div class="container">
+            <h1 style="text-align: center;"><strong>OUR PARTNERSHIP</strong></h1>
+            <br>
+            <div class="row" data-aos="zoom-in">
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="{{ asset('images/swissbel.png') }}" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="{{ asset('images/marriot.png') }}" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="{{ asset('images/Pondok_Indah_Mall..png') }}" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="{{ asset('images/1960s_Gucci_Logo.png') }}" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="{{ asset('images/1280px-Zara_Logo.png') }}" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="{{ asset('images/starbucks.png') }}" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/ Parthership -->
+
+    <!--Team -->
+    <section class="team">
+        <div class="container">
+            <h1 style="text-align: center;"><strong>MEET OUR TEAM</strong></h1>
+            <br>
+            <div class="card-group">
+                <div class="card">
+                    <img src="{{ asset('images/billy.jpeg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Rafael Billy</h5>
+                        <p class="card-text text-center">UI UX Designer & Developer</p>
+                        <div class="social-media">
+                            <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Juneta Julia</h5>
+                        <p class="card-text text-center">UI UX Designer & Developer</p>
+                        <div class="social-media">
+                            <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Nikken Carmelia</h5>
+                        <p class="card-text text-center">UI UX Designer & Developer</p>
+                        <div class="social-media">
+                            <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/Team -->
+
+    <!-- Client -->
+    <section class="clients">
+        <div class="container">
+            <h1 style="text-align: center;"><strong>OUR HAPPY CLIENTS</strong></h1>
+        </div>
+    </section>
+    <!-- /Client -->
+
+    <!-- footer -->
+    <footer>
+        <div class="container">
+            <div class="col-lg-8">
+                <p>SemestaGroup @ 2023. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+    <!-- /footer -->
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript">
+        var nav = document.querySelector('nav');
+
+        window.addEventListener('scroll', function() {
+            if (window.pageYOffset > 100) {
+                nav.classList.add('bg-dark', 'shadow');
+            } else {
+                nav.classList.remove('bg-dark', 'shadow');
+            }
+        });
+    </script>
+</body>
+
+</html>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Register</title>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+        *{
+            font-family: 'Poppins', sans-serif;
+        }
+        .wrapper{
+            background: #ececec;
+            padding: 0 20px 0 20px;
+            background: #303030;
+        }
+        .main{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+        }
+        .side-image{
+            background-image: url("{{ asset('images/event-2.jpg') }}");
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
+            border-radius: 10px 0 0 10px;
+            position: relative;
+        }
+        .row{
+            width:  900px;
+            height:550px;
+            border-radius: 10px;
+            background: #fff;
+            padding: 0px;
+            box-shadow: 5px 5px 10px 1px rgba(0,0,0,0.2);
+            }
+        .text{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+        .text p{
+            color: #fff;
+            font-size: 20px; 
+        }
+        i{
+            font-weight: 400;
+            font-size: 15px;
+        }
+        .right{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+        }
+        .input-box{
+            width: 330px;
+            box-sizing: border-box;
+        }
+        img{
+            width: 35px;
+            position: absolute;
+            top: 30px;
+            left: 30px;
+        }
+        
+        .form {
+            width: 330px;
+            margin: 0 auto;
+        }
+
+        .signin{
+            text-align: center;
+            font-size: small;
+            margin-top: 20px;
+        }
+        span a{
+            text-decoration: none;
+            font-weight: 700;
+            color: #000;
+            transition: .5s;
+        }
+        span a:hover{
+            text-decoration: underline;
+            color: #000;
+        }
+
+        @media only screen and (max-width: 768px){
+            .side-image{
+                border-radius: 10px 10px 0 0;
+            }
+            img{
+                width: 35px;
+                position: absolute;
+                top: 20px;
+                left: 47%;
+            }
+            .text{
+                position: absolute;
+                top: 70%;
+                text-align: center;
+            }
+            .text p, i{
+                font-size: 16px;
+            }
+            .row{
+                max-width:420px;
+                width: 100%;
+            }
+        }
+    </style>
+</head>
+<body>
+  <div class="wrapper">
+    <div class="container main">
+        <div class="row">
+            <div class="col-md-6 side-image">      
+            </div>
+            <div class="col-md-6 right">
+            <form class="form" action="{{ url('login') }}">
+                    @csrf
+                    <div>
+                        <h4 class="mb-3 text-center"><strong>REGISTER</strong></h4>
+                    </div>
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="floatingInput" placeholder="Name" required />
+                        <label for="floatingInput">Name</label>
+                    </div>
+                    
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="floatingInput" placeholder="Email" required />
+                        <label for="floatingInput">Email</label>
+                    </div>
+
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required />
+                        <label for="floatingPassword">Password</label>
+                    </div>
+
+                    <div class="form-floating">
+                        <input type="number" class="form-control" id="floatingInput" placeholder="Phone Number" required />
+                        <label for="floatingInput">Phone Number</label>
+                    </div>
+
+                    <button type="submit" style="width: 100%;" class="btn btn-primary btn-block mb-2 mt-3">
+                        Login
+                    </button>
+                    <div class="signin">
+                        <span><a href="{{ url('login') }}">Already have an account?</a></span>
+                   </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
